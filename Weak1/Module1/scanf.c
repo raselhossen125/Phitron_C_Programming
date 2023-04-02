1e359ff652193506ef8ec9c3a0b9b3fc4c1cb754
rasel-hossen <straselhossen24@gmail.com>
2023-04-03 00:19:28 +0600

    Refacor code

--- Weak1/Module1/scanf.c
+++ Weak1/Module1/scanf.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+// int main()
+// {
+//     char first;
+//     int second;
+//     float third;
+
+//     printf("Enter any charecter : ");
+//     scanf("%c", &first);
+
+//     printf("Enter any integer number : ");
+//     scanf("%d", &second);
+
+//     printf("Enter any float number : ");
+//     scanf("%f", &third);
+
+//     printf("Charecter : %c\nInteger : %d\nFloat : %.2f", first, second, third);
+//     return 0;
+// }
 int main()
 {
-    char first;
-    int second;
-    float third;
-
-    printf("Enter any charecter : ");
-    scanf("%c", &first);
-
-    printf("Enter any integer number : ");
-    scanf("%d", &second);
-
-    printf("Enter any float number : ");
-    scanf("%f", &third);
-
-    printf("Charecter : %c\nInteger : %d\nFloat : %.2f", first, second, third);
-    return 0;
+    int x = 1;
+    if (x)
+    {
+        printf("hi");
+    }
+    else
+    {
+        printf("how are u");
+    }
 }
